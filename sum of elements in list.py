91756e28fe36109c829d49d8b8ce9cc7779cdf0d
Kardasbhavani <bhavanikardas@gmail.com>
2026-05-19 20:35:21 +0530

    Add script to sum elements in a user-provided list
    Implement a script to calculate the sum of elements in a list input by the user.

---
+++ sum of elements in list.py
@@ -0,0 +1,11 @@
+#Take a list input from a user and print sum of elements in the list
+lst=list(map(int,input().split()))
+Sum=0
+for i in lst:
+    Sum+=i
+print(Sum)
+'''
+output:
+2 3 4 5
+14
+'''
