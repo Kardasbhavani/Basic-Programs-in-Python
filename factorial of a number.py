0fb22adcb251d67ecd1ef1c5b53186a4274c5e1d
Kardasbhavani <bhavanikardas@gmail.com>
2026-05-19 20:44:41 +0530

    Create factorial of a number.py

---
+++ factorial of a number.py
@@ -0,0 +1,12 @@
+#To find factorial of a number using recursion
+def factorial(n):
+    if(n==0):
+        return 1
+    return n*factorial(n-1)
+n=int(input())
+print(factorial(n))
+'''
+output:
+5
+120
+'''
