e0c80f5a950be1b9849c44c8a6dd91bf4b5718a1
Kardasbhavani <bhavanikardas@gmail.com>
2026-05-20 11:21:35 +0530

    Implement checksorted function in sorted list.py
    Add function to check if a list is sorted.

---
+++ sorted list.py
@@ -0,0 +1,18 @@
+#To check whether list is sorted or not
+def checksorted(lst):
+    n=len(lst)
+    for i in range(0,n-1):
+        if(lst[i]<lst[i+1]):
+            continue
+        else:
+            return False
+    return True
+lst=list(map(int,input().split()))
+print(checksorted(lst))
+'''
+output:
+1 2 3 0 4
+False
+'''
+            
+
